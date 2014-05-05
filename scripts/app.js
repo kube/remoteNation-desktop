3f67fc963a1b05d0b4fc1d62d151482cfbfeda09
Christophe FEIJOO <cfeijoo@e2r4p2.42.fr>
2014-05-06 00:21:26 +0200

    Update with Page Renderer

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,16 @@
-alert("HELLO WORLD!");
+
+var fs = require('fs');
+var jade = require('jade');
+
+var render = function(viewName, variables)
+{
+	jade.renderFile('views/' + viewName + '.jade', variables,
+		function (err, html) {
+			if (err) throw err;
+			document.getElementsByTagName('html')[0].innerHTML = html;
+		});
+}
+
+$(document).ready(function() {
+	render('index', {pageTitle: "Bonjour tout le monde!"});
+});
